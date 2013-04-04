2382f542979a7b2015c5e9fc5199e9dff0e765f8
rose10 <rosileneserafa@gmail.com>
2013-04-04 10:40:02 -0300

    aula03, unid09.

---
+++ ingles/unid09/au3/top04.php
@@ -0,0 +1,10 @@
+<?php 
+    require("../../../tpl/Template.class.php"); 
+    $tpl = new Template("../../../templates/ingles/unid-09/au-3/top04.tpl"); 
+    $tpl->addFile("tituloAula", "../../../templates/ingles/unid-09/au-3/tituloAula.tpl"); 
+    $tpl->addFile("topo", "../../../topo.tpl"); 
+    $tpl->addFile("barra_tools", "../../../templates/ingles/unid-09/au-3/barra_tools.tpl");
+    $tpl->addFile("menu", "../../../templates/ingles/unid-09/au-3/menu.tpl"); 
+    $tpl->addFile("rodape", "../../../rodape.tpl"); 
+    $tpl->show(); 
+?>
---
+++ ingles/unid09/au3/top07.php
@@ -0,0 +1,10 @@
+<?php 
+    require("../../../tpl/Template.class.php"); 
+    $tpl = new Template("../../../templates/ingles/unid-09/au-3/top07.tpl"); 
+    $tpl->addFile("tituloAula", "../../../templates/ingles/unid-09/au-1/tituloAula.tpl"); 
+    $tpl->addFile("topo", "../../../topo.tpl"); 
+    $tpl->addFile("menu", "../../../templates/ingles/unid-09/au-3/menu.tpl"); 
+    $tpl->addFile("barra_tools", "../../../templates/ingles/unid-09/au-3/barra_tools.tpl");
+    $tpl->addFile("rodape", "../../../rodape.tpl"); 
+    $tpl->show(); 
+?>
